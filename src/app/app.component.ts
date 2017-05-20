2110bd1e938d79b3d2df05baef4f77541c7bf4ed
Delink-D <delinkdesigns@gmail.com>
2017-05-21 00:04:08 +0300

    add of navigation code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'myapp',
-  templateUrl: './app.component.html',
+  templateUrl: 
+  	'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello, Angular:4';
+
+	addPatients = true;
+	viewPatients = false;
+
+	newPatient(){
+		this.addPatients = true;
+		this.viewPatients = false;
+	}
+	viewP(){
+		this.addPatients = false;
+		this.viewPatients = true;
+	}
 }
